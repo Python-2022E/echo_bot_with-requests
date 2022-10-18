a3de69c8e7bb9186af55823a392b88e47e561b4f
Jalilov <javohirjalilov777@gmail.com>
2022-10-18 16:08:56 +0500

    functional

--- main.py
+++ main.py
@@ -1,78 +1,38 @@
 from cgi import test
+from re import A
 import requests
 
 TOKEN='5559122728:AAHOu1gL4pA1riJPMCmICNTKI57P5xnHsyA'
 
 new_update_length = -1
-while True:
+
+def get_updates(TOKEN):
     updates = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
-
     updates = updates.json()
-    last_updates = updates['result'][-1]
+    return updates
 
+def get_last_update(updates):
+    last_updates = updates['result'][-1]
     chat_id = last_updates['message']['chat']['id']
     text = last_updates['message']['text']
-    last_update_length = len(updates['result'])
+    return chat_id,text
 
-    if new_update_length != last_update_length:
-        data = {
+def send_message(TOKEN,chat_id, text):
+    data = {
             'chat_id':chat_id,
             'text':text
         }
     
-        r = requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',data=data)
+    r = requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',data=data)
+
+while True:
+    updates = get_updates(TOKEN)
+    last_update = get_last_update(updates)
+    chat_id, text = last_update
+
+    last_update_length = len(updates['result'])
+
+    if new_update_length != last_update_length:
+        send_message(TOKEN,chat_id=chat_id, text=text)
         new_update_length = last_update_length
-    print("NEW UPDATE LENGTH",new_update_length, "LAST UPDATE LENGTH", last_update_length)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_updates(TOKEN):
-#     r = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
-#     updates = r.json()
-#     return updates
-
-# def send_message(chat_id, text):
-#     data = {
-#         'chat_id':chat_id,
-#         'text':text
-#     }
-#     requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',data=data)
-
-# def get_last_updates(updates):
-
-#     last_updates = updates['result'][-1]
-
-#     chat_id = last_updates['message']['chat']['id']
-#     text = last_updates['message']['text']
-#     return chat_id, text
-
-# new_msg_len = 1
-# while True:
-#     data = get_updates(TOKEN)
-
-#     last_msg_len = len(data['result'])
-    
-#     chat_id,text = get_last_updates(data)
-#     if last_msg_len != new_msg_len:
-#         send_message(chat_id,text)
-
-#         new_msg_len = last_msg_len
+    # print("NEW UPDATE LENGTH",new_update_length, "LAST UPDATE LENGTH", last_update_length)
