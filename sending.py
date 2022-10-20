4ebb76f3c8fed3c09cb22f52c691348fdf2a2402
Jalilov <javohirjalilov777@gmail.com>
2022-10-20 14:54:09 +0500

    send cat image

--- sending.py
+++ sending.py
@@ -1,7 +1,7 @@
 import requests
 
 TOKEN='5559122728:AAHOu1gL4pA1riJPMCmICNTKI57P5xnHsyA'
-url = 'https://krasivosti.pro/uploads/posts/2021-04/1618326499_50-p-malenkaya-martishka-obezyani-krasivo-foto-57.jpg'
+url = 'https://cdn2.thecatapi.com/images/MTY4Njk5Ng.jpg'
 chat_id = 5575549228
 
 data = {
